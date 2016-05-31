26b7e7d4f0a399c8ee1d3c12e63c857ea36d5065
Vevek Selvam <vevekselvam@gmail.com>
2016-05-31 23:39:28 +0800

    Core Location Added: Location working

--- Sunlit/MainViewController.swift
+++ Sunlit/MainViewController.swift
@@ -9,35 +9,68 @@
 
 import UIKit
 import ForecastIO
+import CoreLocation
 
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, CLLocationManagerDelegate {
     
     let forecastIOClient = APIClient(apiKey: "236ecd8bf4a1bcf90ef96c84524268a6")
+    var currentLocationLatitude: Double = 0.0
+    var currentLocationLongitude: Double = 0.0
+    
+    var locationManager: CLLocationManager!
+    
+    
     @IBOutlet weak var Label1: UILabel!
     @IBOutlet weak var Label2: UILabel!
     
+    
+    
+    //MARK: View Did Load START
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        //MARK: Location Manager Initialised START
+        locationManager = CLLocationManager()
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        //MARK: Forecast.io Unit Set up START
         forecastIOClient.units = .SI
         
-        forecastIOClient.getForecast(latitude: 1.352083, longitude: 103.819836) { (currentForecast, error) -> Void in
+        
+        //MARK: Forecast.io Funtion START
+        forecastIOClient.getForecast(latitude: currentLocationLatitude, longitude: currentLocationLongitude) { (currentForecast, error) -> Void in
             if let currentForecast = currentForecast {
                 //  We got the current forecast!
-                self.Label1.text = currentForecast.hourly?.data?.description
-                self.Label2.text = currentForecast.daily?.data?.description
-            } else if let error = error {
-                //  Uh-oh we have an error!
+                self.Label1.text = String(currentForecast.currently?.summary)
+                //self.Label2.text = currentForecast.currently.debugDescription
             }
+            //Handle Error
+            //else if let error = error {Uh-oh we have an error!}
         }
         
         
     }
     
-
+    //MARK: Location Manager Funtion START
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var userLocation:CLLocation = locations[0] as! CLLocation
+        let currentLocationLongitude = userLocation.coordinate.longitude;
+        let currentLocationLatitude = userLocation.coordinate.latitude;
+        
+        //Do What ever you want with it
+        self.Label2.text = String(currentLocationLongitude)
+        
+    }
     
+
+    //MARK: Did Receive Memory Warning START
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
